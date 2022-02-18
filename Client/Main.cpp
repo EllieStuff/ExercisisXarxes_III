67c62dba3748b91c85976dbc5e44ae288f5584ed
Milion16 <joel.mateu@enti.cat>
2022-02-18 12:40:33 +0100

    Relacio Server-Client

--- Client/Main.cpp
+++ Client/Main.cpp
@@ -1,8 +1,42 @@
 
+#include <iostream>
+#include <SFML/Network.hpp>
+//#include <vector>
+//#include <thread>
+//#include <chrono>
 
+// Mirar ip en consola amb ipconfig, sino, es pot fer en mateix PC amb "127.0.0.1" o "localHost"
 
 int main() {
+	sf::TcpSocket sock;
+	sf::Socket::Status status = sock.connect("127.0.0.1", 50000);
+	bool end = false;
 
+	do {
+		sf::Packet pack;
+		status = sock.receive(pack);
+		std::string str;
+
+		switch (status)
+		{
+		case sf::Socket::Done:
+			pack >> str;
+			std::cout << str;
+
+			break;
+
+
+		case sf::Socket::Disconnected:
+			end = true;
+
+			break;
+
+
+		default:
+			break;
+		}
+
+	} while (!end);
 
 
 	return 0;
--- Server/Main.cpp
+++ Server/Main.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 #include <SFML/Network.hpp>
 #include <vector>
+#include <thread>
+#include <chrono>
 
 
-void AceptarConexiones(std::vector<sf::TcpSocket*>* _clientes) {
+void AceptarConexiones(std::vector<sf::TcpSocket*>* _clientes, bool* _end) {
 	sf::TcpListener listener;
 	sf::Socket::Status status = listener.listen(50000);
 	if (status != sf::Socket::Status::Done) {
@@ -12,16 +14,54 @@ void AceptarConexiones(std::vector<sf::TcpSocket*>* _clientes) {
 	}
 
 
-	while (true) {
-		listener.accept
-
+	while (!(*_end)) {
 		//Accept
-		
-		//Guardar en clients
+		sf::TcpSocket* sock = new sf::TcpSocket();
+		status = listener.accept(*sock);
+		if (status != sf::Socket::Status::Done) {
+			delete sock;
+			continue;
+		}
 
+		//Guardar en clients
+		_clientes->push_back(sock);
 
 	}
 
+}
+
+void EnvioPeriodico(std::vector<sf::TcpSocket*>* _clientes, bool* _end) {
+	sf::Packet pack;
+	std::string str = "Mensaje desde el servidor\n";
+	pack << str;
+
+	while (!(*_end)) {
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		for (size_t i = 0; i < _clientes->size(); i++) {
+			sf::Socket::Status status = _clientes->at(i)->send(pack);
+			switch (status)
+			{
+			case sf::Socket::Done:
+				continue;
+
+
+			case sf::Socket::Disconnected:
+				_clientes->at(i)->disconnect();
+				delete _clientes->at(i);
+				_clientes->erase(_clientes->begin() + i);
+				i--;
+
+				break;
+
+
+			default:
+				break;
+
+			}
+
+		}
+
+	}
 }
 
 
@@ -29,9 +69,16 @@ int main() {
 	std::vector<sf::TcpSocket*> clientes;
 	char opc;
 	bool end = false;
+	std::thread tAccepts(AceptarConexiones, &clientes, &end);
+	tAccepts.detach();
+	std::thread tSends(EnvioPeriodico, &clientes, &end);
+	tSends.detach();
+
 	do {
 		std::cin >> opc;
-	} while (!end);
+	} while (opc != 'e');
+
+	end = true;
 
 
 	return 0;
