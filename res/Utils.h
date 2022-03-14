e0025ae85a83d8b4bc0dbea22bced26ec826a806
Mílion <joel.mateu@enti.cat>
2022-03-14 13:23:01 +0100

    creant structs

--- res/Utils.h
+++ res/Utils.h
@@ -9,3 +9,24 @@ struct PeerAddress {
 	int port;
 };
 
+
+struct Card {
+private:
+	enum class CardType { ORGAN, MEDICINE, VIRUS, TREATMENT };
+	enum class OrganType { STOMACH, BRAIN, SKELETON, HEART, NONE };
+	enum class TreatmentType { INFECTION, ROBER, TRANSPLANT, LATEX_GLOVES, MEDICAL_ERROR, NONE };
+
+public:
+	CardType cardType;
+	OrganType organType;
+	TreatmentType treatmentType;
+	bool isWildcard = false;
+};
+
+struct VirusData {
+	int playerId;
+	int currTurn;
+	bool hasStolen;	// Nota: passar cartes al principi de la principi
+	// ToDo?
+};
+
