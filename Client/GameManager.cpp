b6f4be6fe166e328840e1c27d6d3eb2598ba1de1
xavi <30593493+xdman21@users.noreply.github.com>
2022-03-21 12:10:45 +0100

    ª

--- Client/GameManager.cpp
+++ Client/GameManager.cpp
@@ -93,7 +93,7 @@ GameManager::~GameManager()
 
 bool GameManager::Update()
 {
-	if (*currentTurn == playerTurnOrder.size()|| playerTurnOrder[*currentTurn].playerID != player->id)
+	if (*currentTurn == playerTurnOrder.size() || playerTurnOrder[*currentTurn].playerID != player->id)
 		return *endRound;
 
 	std::cout << "" << std::endl;
