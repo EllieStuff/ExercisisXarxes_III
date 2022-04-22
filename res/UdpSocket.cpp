b6abfb876a072596b5fba1f1a90f29f1c2354c5a
Jordi <jordi.herrera@enti.cat>
2022-04-22 13:18:03 +0200

    un canvi

--- res/UdpSocket.cpp
+++ res/UdpSocket.cpp
@@ -1,1 +1,24 @@
 #include "UdpSocket.h"
+
+UdpSocket::UdpSocket()
+{
+}
+
+UdpSocket::~UdpSocket()
+{
+}
+
+void UdpSocket::Send(OutputMemoryStream* out, Status& status)
+{
+}
+
+InputMemoryStream* UdpSocket::Receive(Status& status)
+{
+	InputMemoryStream* in;
+
+	return in;
+}
+
+void UdpSocket::Bind()
+{
+}
