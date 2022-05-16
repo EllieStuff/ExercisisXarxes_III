99aa847a3b8164f26ce86db7832e4cf165e9cd1f
xavi <30593493+xdman21@users.noreply.github.com>
2022-05-16 13:11:12 +0200

    a

--- Server/ClientData.cpp
+++ Server/ClientData.cpp
@@ -11,8 +11,6 @@ void ClientData::GenerateSALT(int& value, int numOfBytes = 4)
 
 ClientData::ClientData(unsigned short _port, IpAddress _address, std::string _name, int _salt): port(_port), address(_address), name(_name), clientSALT(_salt)
 {
-	timeStamp = clock();
-
 	GenerateSALT(serverSALT);
 }
 
